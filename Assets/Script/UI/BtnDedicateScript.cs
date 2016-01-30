af13e143552074e8b207e37bcfc96a050d218b17
OMD_sadakitchen <contact@onemoredot.com>
2016-01-30 18:16:57 +0900

    メインUI追加

---
+++ Assets/Script/UI/BtnDedicateScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BtnDedicateScript : MonoBehaviour {
+
+	public GameObject _self;
+
+	// Use this for initialization
+	void Start () {
+		// 初め消しておく
+		hide();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	// 
+	public void dedicate(){
+		Debug.Log ("BtnDedicateScript :: dedicate");
+	}
+
+	// 
+	public void show(){
+		_self.SetActive (true);
+	}
+
+	// 
+	public void hide(){
+		_self.SetActive (false);
+	}
+}
---
+++ Assets/Script/UI/UICanvasScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UICanvasScript : MonoBehaviour {
+
+	// itemlist
+	private int[] itemArray = new int[]{ 0, 0, 0, 0, 0};
+
+	// itemlist
+	public GameObject[] spriteOffArray = new GameObject[5];
+	public GameObject[] spriteOnArray = new GameObject[5];
+
+	private List<int> items = new List<int>();
+
+	// Use this for initialization
+	void Start () {
+		items.AddRange(itemArray);
+
+		showItems();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	// 
+	public void getItem( int _index){
+		itemArray [_index] = 1;
+		showItems ();
+	}
+
+	// 
+	public void lostItem( int _index){
+		itemArray [_index] = 0;
+		showItems ();
+	}
+
+	// 
+	private void showItems(){
+		for (int i = 0; i < items.Count; i++) {
+			spriteOnArray [i].SetActive (false);
+			spriteOffArray [i].SetActive (false);
+			if (0 < items [i]) {
+				spriteOnArray [i].SetActive (true);
+			} else {
+				spriteOffArray [i].SetActive (true);
+			}
+		}
+	}
+}
