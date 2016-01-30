3b5214ea18dac507dd2a6ad9cffa9099f7f751e6
OMD_sadakitchen <contact@onemoredot.com>
2016-01-30 21:21:40 +0900

    基本UI作成

--- Assets/Script/UI/UICanvasScript.cs
+++ Assets/Script/UI/UICanvasScript.cs
@@ -15,17 +15,27 @@ public class UICanvasScript : MonoBehaviour {
 	// itemlist
 	public GameObject[] spriteOffArray = new GameObject[5];
 	public GameObject[] spriteOnArray = new GameObject[5];
+	public GameObject[] spriteHudaArray = new GameObject[5];
+	public GameObject[] spriteEndArray = new GameObject[5];
 
 	private List<int> items = new List<int>();
 
-	// HP
+	// HPゲージ
 	public ImgHpGaugeScript gauge;
+	// HP枠
+	public WrapperGaugeScript frame;
 	// お供えグラフィック
 	public BtnDedicateScript dedicateInfo;
 
 	// Use this for initialization
 	void Start () {
 		items.AddRange(itemArray);
+
+		// お供え消す
+		showDedicate (false);
+
+		// HPセット
+		setHp(30,100);
 
 		showItems();
 	}
@@ -36,31 +46,47 @@ public class UICanvasScript : MonoBehaviour {
 	}
 
 	// 
-	public void getItem( int _index){
-		itemArray [_index] = 1;
-		showItems ();
-	}
-
-	// 
-	public void lostItem( int _index){
-		itemArray [_index] = 0;
-		showItems ();
-	}
-
-	// 
 	private void showItems(){
 		for (int i = 0; i < items.Count; i++) {
 			spriteOnArray [i].SetActive (false);
 			spriteOffArray [i].SetActive (false);
-			if (0 < items [i]) {
-				spriteOnArray [i].SetActive (true);
-			} else {
-				spriteOffArray [i].SetActive (true);
+			spriteHudaArray [i].SetActive (false);
+			spriteEndArray [i].SetActive (false);
+
+			// アイテムの状態で表示状態を変更
+			switch(items [i]){
+			case 1:// 1:供物所持
+					spriteOnArray [i].SetActive (true);
+					break;
+			case 2:// 2:札所持
+					spriteHudaArray [i].SetActive (true);
+					break;
+			case 3:// 3:札使用
+					spriteEndArray [i].SetActive (true);
+					break;
+			default:// 0:未所持
+					spriteOffArray [i].SetActive (true);
+					break;
 			}
 		}
 	}
 
-	// お供え表示
+	/**
+	 * アイテムの表示状態
+	 * 
+	 * @param	_index	int
+	 * @param	_state	int
+	 */
+	public void updateItem( int _index, int _state ){
+		itemArray [_index] = _state;
+		showItems ();
+	}
+
+	/**
+	 * お供え表示
+	 * 
+	 * @param	_bool	bool
+	 */
 	public void showDedicate( bool _bool){
 		if(_bool){
 			dedicateInfo.show ();
@@ -69,8 +95,14 @@ public class UICanvasScript : MonoBehaviour {
 		}
 	}
 
-	// HPセット
+	/**
+	 * HPセット
+	 * 
+	 * @param	_now	int
+	 * @param	_max	int
+	 */
 	public void setHp( int _now, int _max){
 		gauge.setHp (_now, _max);
+		frame.checkDanger(_now, _max);
 	}
 }
