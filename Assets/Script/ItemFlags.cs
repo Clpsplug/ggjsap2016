8a9482d0a1ee630c24303a37dfb65d47552fb59e
unknown <sysop@iga111.iga.edu.jp>
2016-01-31 01:47:05 +0900

    プレイヤーコントローラーの調整（未完了）

--- Assets/Script/ItemFlags.cs
+++ Assets/Script/ItemFlags.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class ItemFlags : MonoBehaviour {
 
+    public string now_col;
+
     public static int[] Items = new int[5];
     // 1果物
     // 2お酒
@@ -10,10 +12,10 @@ public class ItemFlags : MonoBehaviour {
     // 4魚
     // 5刀
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    // Use this for initialization
+    void Start() {
+
+    }
 
     // Update is called once per frame
     void Update()
@@ -21,91 +23,85 @@ public class ItemFlags : MonoBehaviour {
 
     }
 
-    void OnCollisionEnter (Collision col)
+    void OnCollisionStay(Collision col)
     {
-        // Debug.Log(col.gameObject.tag);
+        now_col = col.gameObject.tag.ToString();
         switch (col.gameObject.tag.ToString())
         {
             case "orange":
                 //果物
                 Items[0] = 1;
-                Debug.Log(Items[0]+col.gameObject.tag);
+                //Debug.Log(Items[0]+col.gameObject.tag);
                 break;
 
             case "osake":
                 //お酒
                 Items[1] = 1;
-                Debug.Log(Items[1] + col.gameObject.tag);
+                //Debug.Log(Items[1] + col.gameObject.tag);
                 break;
 
             case "salt":
                 //塩
                 Items[2] = 1;
-                Debug.Log(Items[2] + col.gameObject.tag);
+                //Debug.Log(Items[2] + col.gameObject.tag);
                 break;
 
             case "fish":
                 //魚
                 Items[3] = 1;
-                Debug.Log(Items[3] + col.gameObject.tag);
+                //Debug.Log(Items[3] + col.gameObject.tag);
                 break;
 
             case "katana":
                 //刀
                 Items[4] = 1;
-                Debug.Log(Items[4] + col.gameObject.tag);
+                //Debug.Log(Items[4] + col.gameObject.tag);
                 break;
-        }
 
-        if (Input.GetKey("q"))
-        {
-            switch (col.gameObject.tag.ToString())
-            {
-                case "izanami":
-                    Debug.Log(col.gameObject.tag);
-                    if (ItemFlags.Items[0] == 1)
-                    {
-                        ItemFlags.Items[0] = 2;
-                        Debug.Log("izanami2");
-                    }
-                    break;
+            case "izanami":
+                Debug.Log(col.gameObject.tag);
+                if (ItemFlags.Items[0] == 1 && Input.GetKey("z"))
+                {
+                    ItemFlags.Items[0] = 2;
+                    Debug.Log("izanami2");
+                }
+                break;
 
-                case "sukunahiko":
-                    Debug.Log(col.gameObject.tag);
-                    if (ItemFlags.Items[1] == 1)
-                    {
-                        ItemFlags.Items[1] = 2;
-                        Debug.Log("sukunahiko2");
-                    }
-                    break;
+            case "sukunahiko":
+                Debug.Log(col.gameObject.tag);
+                if (ItemFlags.Items[1] == 1 && Input.GetKey("z"))
+                {
+                    ItemFlags.Items[1] = 2;
+                    Debug.Log("sukunahiko2");
+                }
+                break;
 
-                case "shiotsuti":
-                    Debug.Log(col.gameObject.tag);
-                    if (ItemFlags.Items[2] == 1)
-                    {
-                        ItemFlags.Items[2] = 2;
-                        Debug.Log("shiotsuti2");
-                    }
-                    break;
+            case "shiotsuti":
+                Debug.Log(col.gameObject.tag);
+                if (ItemFlags.Items[2] == 1 && Input.GetKey("z"))
+                {
+                    ItemFlags.Items[2] = 2;
+                    Debug.Log("shiotsuti2");
+                }
+                break;
 
-                case "isotakeru":
-                    Debug.Log(col.gameObject.tag);
-                    if (ItemFlags.Items[3] == 1)
-                    {
-                        ItemFlags.Items[3] = 2;
-                        Debug.Log("isotakeru2");
-                    }
-                    break;
+            case "isotakeru":
+                Debug.Log(col.gameObject.tag);
+                if (ItemFlags.Items[3] == 1 && Input.GetKey("z"))
+                {
+                    ItemFlags.Items[3] = 2;
+                    Debug.Log("isotakeru2");
+                }
+                break;
 
-                case "takeminakata":
-                    Debug.Log(col.gameObject.tag);
-                    if (ItemFlags.Items[4] == 1)
-                    {
-                        ItemFlags.Items[4] = 2;
-                        Debug.Log("takeminakata2");
-                    }
-                    break;
-            }
+            case "takeminakata":
+                Debug.Log(col.gameObject.tag);
+                if (ItemFlags.Items[4] == 1 && Input.GetKey("z"))
+                {
+                    ItemFlags.Items[4] = 2;
+                    Debug.Log("takeminakata2");
+                }
+                break;
         }
     }
 }
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -3,31 +3,55 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    public float speed = 10.0f;
+    public float speed;
+    public int move_flag = 1; //UIから引数をもらう予定
+    public UICanvasScript UI;
+    public SceneControllerScript scene;
+
+    private int max_hp = 500;
+    private int now_hp = 0; 
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        now_hp = max_hp;
+        UI.setHp(now_hp, max_hp);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-
-        if (Input.GetKey("w"))
+        if (move_flag == 1)
         {
-            gameObject.GetComponent<Transform>().position += transform.forward * Time.deltaTime * speed;
-        }
+            speed = 10.0f;
+            if (Input.GetKey("w"))
+            {
+                gameObject.GetComponent<Transform>().position += transform.forward * Time.deltaTime * speed;
+                if (0 < now_hp)
+                {
+                    now_hp--;
+                }
+                else if (now_hp <= 0)
+                {
+                    // gameover
+                    scene.showScene(4);
+                }
+                UI.setHp(now_hp, max_hp);
 
-        if (Input.GetKey("d"))
-        {
-            // Debug.Log("dKey");
-            gameObject.GetComponent<Transform>().Rotate(0, 50.0f*Time.deltaTime, 0);
-        }
+            }
 
-        if (Input.GetKey("a"))
+            if (Input.GetKey("d"))
+            {
+                // Debug.Log("dKey");
+                gameObject.GetComponent<Transform>().Rotate(0, 50.0f * Time.deltaTime, 0);
+            }
+
+            if (Input.GetKey("a"))
+            {
+                //Debug.Log("aKey");
+                gameObject.GetComponent<Transform>().Rotate(0, -50.0f * Time.deltaTime, 0);
+            }
+        } else
         {
-            //Debug.Log("aKey");
-            gameObject.GetComponent<Transform>().Rotate(0, -50.0f*Time.deltaTime, 0);
+            speed = 0;
         }
     }
 }
