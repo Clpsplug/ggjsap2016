a852a0abcbb333c69452692590a99e563f7503f7
unknown <sysop@iga111.iga.edu.jp>
2016-01-30 17:00:02 +0900

    交換用の配列確認クラス、アイテムの取得時消滅用クラス、プレイヤーコントローラの改修

---
+++ Assets/ExchangeItem.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExchangeItem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (Input.GetKey("q"))
+        {
+            switch (col.gameObject.tag.ToString())
+            {
+                case "izanami":
+                    if(ItemFlags.Items[1] == 1)
+                    {
+                        ItemFlags.Items[1] = 2;
+                    }
+                    break;
+
+                case "isotakeru":
+                    if (ItemFlags.Items[2] == 1)
+                    {
+                        ItemFlags.Items[2] = 2;
+                    }
+                    break;
+
+                case "sukunahiko":
+                    if (ItemFlags.Items[3] == 1)
+                    {
+                        ItemFlags.Items[3] = 2;
+                    }
+                    break;
+
+                case "shiotsutsi":
+                    if (ItemFlags.Items[4] == 1)
+                    {
+                        ItemFlags.Items[4] = 2;
+                    }
+                    break;
+
+                case "takeminakata":
+                    if (ItemFlags.Items[5] == 1)
+                    {
+                        ItemFlags.Items[5] = 2;
+                    }
+                    break;
+            }
+        }
+    }
+}
