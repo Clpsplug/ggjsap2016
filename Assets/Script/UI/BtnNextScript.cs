bd861b8af27398bfb32ab3973f6a140afb53acc8
OMD_sadakitchen <contact@onemoredot.com>
2016-01-30 23:07:43 +0900

    ストーリー画面追加

---
+++ Assets/Script/UI/BtnNextScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BtnNextScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// 次へ
+	public void pressBtn(){
+		Debug.Log ("BtnNextScript :: pressBtn");
+	}
+}
---
+++ Assets/Script/UI/StoryViewScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StoryViewScript : MonoBehaviour {
+
+	// ストーリ用テキストエリア
+	public Text textfield;
+
+	// 次へボタン
+	public BtnNextScript btn_next;
+
+	// テキスト
+	[Multiline(3)]
+	public string[] storyArray = new string[5];
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
