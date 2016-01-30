9eccedd2a46080533d06e4f037e9238794842921
Igarashi1996 <t.igarashi1996@gmail.com>
2016-01-30 17:01:53 +0900

    迷路の生成スクリプトとResourceの追加

---
+++ Assets/Script/CreateWall.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CreateWall : MonoBehaviour {
+    private const int SIDE_LENGTH = 6;                  //マップの一辺の柱の本数(偶数のみです、すみません)
+    private const int WALL_LENGHT = SIDE_LENGTH - 1;     //マップの一辺の壁の数
+
+    private GameObject mPillarObj;                                  //柱
+    private GameObject mWidthWallObj;                               //横壁
+    private GameObject mHeightWallObj;                              //縦壁
+    private GameObject mWalls;                                      //壁の親オブジェクト
+
+    private int mUpParentIndex = 0;                                 //上壁の指標
+    private int mLeftParentIndex = 0;                               //左壁の指標
+    private int mRightParentIndex = 0;                              //右壁の指標
+    private int mUnderParentIndex = 0;                              //左壁の指標
+
+    private int mWallsNum = 0;                                      //壁の親オブジェクトの数
+    private GameObject[] mWallsParentArray =
+                            new GameObject[SIDE_LENGTH * SIDE_LENGTH / 2 - (SIDE_LENGTH / 2)];
+                                                                    //壁の親オブジェクトを格納する配列
+
+    // Use this for initialization
+    void Start () {
+        mPillarObj = (GameObject)Resources.Load("Pillar");
+        mWidthWallObj = (GameObject)Resources.Load("WidthWall");
+        mHeightWallObj = (GameObject)Resources.Load("HeightWall");
+        mWalls = (GameObject)Resources.Load("Walls");
+
+        for (int i = 0; i < SIDE_LENGTH * SIDE_LENGTH / 2 - (SIDE_LENGTH / 2); i++)               // 壁の親オブジェクトの生成
+        {
+            GameObject walls = (GameObject)Instantiate(mWalls, Vector3.zero, new Quaternion());
+            walls.name = "Walls" + mWallsNum;
+            walls.transform.parent = GameObject.Find("WallParent").transform;
+            mWallsParentArray[mWallsNum] = walls;
+            mWallsNum++;
+        }
+        mWallsNum = 0;
+
+        for (int i = 1; i <= SIDE_LENGTH; i++)                      //柱の生成
+        {
+            for (int j = 1; j <= SIDE_LENGTH; j++)
+            {
+                GameObject pillar = (GameObject)Instantiate(mPillarObj, new Vector3((j - 1) * 5 + 2.5f, 1, (i - 1) * 5 + 2.5f), new Quaternion(0, 0, 0, 0));
+                pillar.name = "Pillar" + i + "," + j;
+                pillar.transform.parent = GameObject.Find("PillarParent").transform;
+            }
+        }
+        for (int i = 1; i <= SIDE_LENGTH; i++)                      //壁の生成
+        {
+            for(int j = 1; j <= WALL_LENGHT; j++)
+            {
+                GameObject widthWall = (GameObject)Instantiate(mWidthWallObj, new Vector3(j * 5, 1, (i - 1) * 5 + 2.5f), new Quaternion());         //横壁の生成
+                if (i % 2 == 1)                         //奇数ならUnder,偶数ならUp
+                {
+                    widthWall.name = "Under";
+                    widthWall.transform.parent = mWallsParentArray[mUnderParentIndex].transform;
+                    mUnderParentIndex++;
+
+                    if (i == 1)
+                    {
+                        widthWall.name = "MinUnder";
+                    }
+                }
+                else
+                {
+                    widthWall.name = "Up";
+                    widthWall.transform.parent = mWallsParentArray[mUpParentIndex].transform;
+                    mUpParentIndex++;
+
+                    if (i == SIDE_LENGTH)
+                    {
+                        widthWall.name = "MaxUp";
+                    }
+                }
+
+                GameObject heightWall = (GameObject)Instantiate(mHeightWallObj, new Vector3((i - 1) * 5 + 2.5f, 1, j * 5 ), new Quaternion());      //縦壁の生成
+                if (i % 2 == 1)                         //奇数ならLeft,偶数ならRight
+                {
+                    heightWall.name = "Left";
+                    heightWall.transform.parent = mWallsParentArray[mLeftParentIndex].transform;
+                    mLeftParentIndex++;
+
+                    if (i == 1)
+                    {
+                        widthWall.name = "MinLeft";
+                    }
+                }
+                else
+                {
+                    heightWall.name = "Right";
+                    heightWall.transform.parent = mWallsParentArray[mRightParentIndex].transform;
+                    mRightParentIndex++;
+
+                    if (i == SIDE_LENGTH)
+                    {
+                        widthWall.name = "MaxRight";
+                    }
+                }
+
+                
+                
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Script/RandomWall.cs
@@ -0,0 +1,166 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class RandomWall : MonoBehaviour {
+
+    private int mWallsNumber;                                       //壁の親オブジェクト"Walls"の数
+    private int mSeedValue = Environment.TickCount;                 //シード値
+    private int[] mRandomArray;                                     //乱数の値を入れる配列
+    private GameObject mWalls;                                      //壁の親オブジェクト"Walls"
+
+    private bool mInitFlag = true;                                  //生成された"Walls"のカウント用
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (mInitFlag)
+        {
+            mWallsNumber = GameObject.Find("WallParent").transform.childCount;      //"Walls"の数を取得
+            mRandomArray = new int[mWallsNumber];                                   //"Walls"の数だけ配列を確保
+
+            System.Random rnd = new System.Random(mSeedValue++);                    //乱数
+            for (int i = 0; i < mWallsNumber; i++)
+            {
+                mRandomArray[i] = rnd.Next(5);                                      //乱数を取得
+            }
+
+            for (int i = 0; i < mWallsNumber; i++)                                  //"Walls"の数だけ乱数を評価
+            {
+                mWalls = GameObject.Find("Walls" + i);
+                switch (mRandomArray[i])
+                {
+                    case 0:
+                        HideWall("Up");
+                        HideWall("Left");
+                        break;
+
+                    case 1:
+                        HideWall("Up");
+                        HideWall("Right");
+                        break;
+
+                    case 2:
+                        HideWall("Up");
+                        HideWall("Under");
+                        break;
+
+                    case 3:
+                        HideWall("Left");
+                        HideWall("Right");
+                        break;
+
+                    case 4:
+                        HideWall("Left");
+                        HideWall("Under");
+                        break;
+
+                    case 5:
+                        HideWall("Right");
+                        HideWall("Under");
+                        break;
+                }
+            }
+            mInitFlag = false;
+        }
+        if (Input.GetKey(KeyCode.A))                                                //デバッグ用マップ生成
+        {
+            System.Random rnd = new System.Random(mSeedValue++);
+            for (int i = 0; i < mWallsNumber; i++)
+            {
+                mWalls = GameObject.Find("Walls" + i);
+                HideWall("Reset");
+                mRandomArray[i] = rnd.Next(5);
+                mInitFlag = true;
+            }
+        }
+    }
+
+    void HideWall(string inWall)
+    {
+        switch (inWall)
+        {
+            case "Up":
+                if (mWalls.transform.FindChild("Up"))
+                {
+                    mWalls.transform.FindChild("Up").GetComponent<BoxCollider>().enabled = false;
+                    mWalls.transform.FindChild("Up").GetComponent<MeshRenderer>().enabled = false;
+                }
+                break;
+
+            case "Left":
+                if (mWalls.transform.FindChild("Left"))
+                {
+                    mWalls.transform.FindChild("Left").GetComponent<BoxCollider>().enabled = false;
+                    mWalls.transform.FindChild("Left").GetComponent<MeshRenderer>().enabled = false;
+                }
+                break;
+
+            case "Right":
+                if (mWalls.transform.FindChild("Right"))
+                {
+                    mWalls.transform.FindChild("Right").GetComponent<BoxCollider>().enabled = false;
+                    mWalls.transform.FindChild("Right").GetComponent<MeshRenderer>().enabled = false;
+                }
+                break;
+
+            case "Under":
+                if (mWalls.transform.FindChild("Under"))
+                {
+                    mWalls.transform.FindChild("Under").GetComponent<BoxCollider>().enabled = false;
+                    mWalls.transform.FindChild("Under").GetComponent<MeshRenderer>().enabled = false;
+                }
+                break;
+
+            case "Reset":
+                if (mWalls.transform.FindChild("Up"))
+                {
+                    mWalls.transform.FindChild("Up").GetComponent<BoxCollider>().enabled = true;
+                    mWalls.transform.FindChild("Up").GetComponent<MeshRenderer>().enabled = true;
+                }
+                else
+                {
+                    mWalls.transform.FindChild("MaxUp").GetComponent<BoxCollider>().enabled = true;
+                    mWalls.transform.FindChild("MaxUp").GetComponent<MeshRenderer>().enabled = true;
+                }
+
+                if (mWalls.transform.FindChild("Left"))
+                {
+                    mWalls.transform.FindChild("Left").GetComponent<BoxCollider>().enabled = true;
+                    mWalls.transform.FindChild("Left").GetComponent<MeshRenderer>().enabled = true;
+                }
+                else
+                {
+                    mWalls.transform.FindChild("MinLeft").GetComponent<BoxCollider>().enabled = true;
+                    mWalls.transform.FindChild("MinLeft").GetComponent<MeshRenderer>().enabled = true;
+                }
+
+                if (mWalls.transform.FindChild("Right"))
+                {
+                    mWalls.transform.FindChild("Right").GetComponent<BoxCollider>().enabled = true;
+                    mWalls.transform.FindChild("Right").GetComponent<MeshRenderer>().enabled = true;
+                }
+                else
+                {
+                    mWalls.transform.FindChild("MaxRight").GetComponent<BoxCollider>().enabled = true;
+                    mWalls.transform.FindChild("MaxRight").GetComponent<MeshRenderer>().enabled = true;
+                }
+
+                if (mWalls.transform.FindChild("Under"))
+                {
+                    mWalls.transform.FindChild("Under").GetComponent<BoxCollider>().enabled = true;
+                    mWalls.transform.FindChild("Under").GetComponent<MeshRenderer>().enabled = true;
+                }
+                else
+                {
+                    mWalls.transform.FindChild("MinUnder").GetComponent<BoxCollider>().enabled = true;
+                    mWalls.transform.FindChild("MinUnder").GetComponent<MeshRenderer>().enabled = true;
+                }
+                break;
+        }
+    }
+}
