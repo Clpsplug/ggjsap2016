9cd6be4c9f93e8a99369d77fe6187b2899fa2f2d
unknown <sysop@iga111.iga.edu.jp>
2016-01-31 09:07:06 +0900

    移動関係のスクリプトの関数化による改修

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,59 +5,79 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public float rotate_speed;
     public int move_flag = 1; //UIから引数をもらう予定
     public UICanvasScript UI;
     public SceneControllerScript scene;
 
     private int max_hp = 500;
-    private int now_hp = 0; 
+    private int now_hp = 0;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         now_hp = max_hp;
         UI.setHp(now_hp, max_hp);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
         if (move_flag == 1)
         {
             speed = 10.0f;
+            rotate_speed = 2.0f;
             if (Input.GetKey("w"))
             {
-                gameObject.GetComponent<Transform>().position += transform.forward * Time.deltaTime * speed;
-                if (0 < now_hp)
-                {
-                    now_hp--;
-                }
-                else if (now_hp <= 0)
-				{
-					move_flag = 0;
-                    // gameover
-					scene.showScene(Define.UI_GAMEOVER_MAIN);
-					UI.myEvent.AddListener (restart);
-				}
-                UI.setHp(now_hp, max_hp);
-
+                forward();
             }
 
             if (Input.GetKey("d"))
             {
-                // Debug.Log("dKey");
-                gameObject.GetComponent<Transform>().Rotate(0, 50.0f * Time.deltaTime, 0);
+                right_turn();
             }
 
             if (Input.GetKey("a"))
             {
-                //Debug.Log("aKey");
-                gameObject.GetComponent<Transform>().Rotate(0, -50.0f * Time.deltaTime, 0);
+                left_turn();
             }
-        } else
+        }
+        else
         {
             speed = 0;
+            rotate_speed = 0;
         }
+
+
     }
 
+    void forward()
+    {
+        gameObject.GetComponent<Transform>().position += transform.forward * Time.deltaTime * speed;
+        if (0 < now_hp)
+        {
+            now_hp--;
+        }
+        else if (now_hp <= 0)
+        {
+            move_flag = 0;
+            // gameover
+            scene.showScene(Define.UI_GAMEOVER_MAIN);
+            UI.myEvent.AddListener(restart);
+        }
+        UI.setHp(now_hp, max_hp);
+    }
+
+    void right_turn()
+    {
+        //Debug.Log("dKey");
+        gameObject.GetComponent<Transform>().Rotate(0, rotate_speed, 0);
+    }
+
+    void left_turn()
+    {
+        //Debug.Log("aKey");
+        gameObject.GetComponent<Transform>().Rotate(0, -rotate_speed, 0);
+    }
+ 
 	/**
 	 * 	リスタート処理
 	 */
