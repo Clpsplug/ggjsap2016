782bdc90bc1d5e18956e9568bade5408114ff93d
unknown <sysop@iga111.iga.edu.jp>
2016-01-31 13:38:30 +0900

    クリア画面への遷移を追加

--- Assets/Script/ItemFlags.cs
+++ Assets/Script/ItemFlags.cs
@@ -149,6 +149,7 @@ public class ItemFlags : MonoBehaviour {
 
             if (i == 5)
             {
+                scene.showScene(Define.UI_CLEAR_MAIN);
                 Debug.Log("game_clear");
             }
             else
